80f0f4a347b48fa150ea89a7011ad97aca0d318e
Sangkeun Kim <sisobus1@gmail.com>
2018-02-19 11:24:20 +0900

    Delete config.py

--- ict_law/config.py
+++
@@ -1,5 +0,0 @@
-__author__ = 'sisobus'
-UPLOAD_FOLDER = '/var/www/ict_law/ict_law/static/files'
-ICT_DATABASE_URI   = 'mysql+pymysql://root:tkdrmsld123@58.227.42.161/ict_law'
-ICT_SECRET_KEY     = 'SET T0 4NY SECRET KEY L1KE RAND0M H4SH'
-
